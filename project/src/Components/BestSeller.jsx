d2c6d5e5807696207ed3f87dceac58d072d6dfa0
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-07 01:17:07 +0530

    cartHome

---
+++ project/src/Components/BestSeller.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cart from './Cart'
+
+const BestSeller = () => {
+  return (
+    <div className='mt-16'>
+        <p className='text-2xl md:text-3xl font-medium'>Best Seller</p>
+       <div>
+        <Cart/>
+       </div>
+    </div>
+  )
+}
+
+export default BestSeller
