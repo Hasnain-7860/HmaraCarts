ad860fddc63594a3055a814ba50703f9769660cf
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-09 16:15:10 +0530

    productclick

--- project/src/Pages/Productcategory.jsx
+++ project/src/Pages/Productcategory.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAppContext } from '../Context/Appcontext'
 import { useParams } from 'react-router-dom'
 import { categories } from '../assets/assets'
+import Cart from '../Components/Cart'
 
 const Productcategory = () => {
     const {products} = useAppContext()
@@ -15,6 +16,17 @@ const Productcategory = () => {
             <div className='flex flex-col items-end w-max'>
                 <p className='text-2xl font-medium'>{searchCategory.text.toUpperCase()}</p>
                 <div className='w-16 h-0.5 bg-green-400 rounded-full'></div>
+            </div>
+        )}
+        {filteredProducts.length > 0 ? (
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+                {filteredProducts.map((product)=>(
+                    <Cart key={product._id} product={product}/>
+                ))}
+            </div>
+        ) : (
+            <div className='flex items-center justify-center h-[60vh]'>
+                <p className='text-2xl font-medium text-green-300'>No products found in this category.</p>
             </div>
         )}
     </div>
