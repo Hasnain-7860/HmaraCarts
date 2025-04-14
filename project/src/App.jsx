2073bf306b2caa065c8c71a899606b3c44115432
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-04-14 23:42:28 +0530

    my order page

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
           {/* <Route path='/cart' element={<Caart/>}/> */}
           <Route path='/Cart' element={<Cart/>}/>
           <Route path='/add-address' element={<AddAddress/>}/>
-          <Route path='/my-orders'element={<MyOrders/>}/>
+          <Route path='/my-orders' element={<MyOrders/>}/>
 
         </Routes>
       </div>
