f90f842ce34dfea6073d22276ea64fd8b476661c
Hasnain-7860 <154454618+Hasnain-7860@users.noreply.github.com>
2025-06-06 16:00:35 +0530

    icon logoprofile

--- project/src/Components/Navbar.jsx
+++ project/src/Components/Navbar.jsx
@@ -61,9 +61,13 @@ const Navbar = () => {
             <button className="absolute -top-2 -right-3 text-xs text-white bg-green-500 w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
         </div>
         </div>
+          
+      <img className='w-10' src={assets.profile_icon} alt="" />
+
 
     <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
         {/* Menu Icon SVG */}
+        
        <img src={assets.menu_icon} alt="" />
     </button>
 
